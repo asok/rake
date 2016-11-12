3e459c8b1df19838f54f48f6c56be0754654db51
Philipp Fehre <philipp@fehre.co.uk>
2016-11-12 19:36:10 +0000

    Refactor to built in function
    `locate-dominating-file` can be used to find a file in a hierarchy.

--- rake.el
+++ rake.el
@@ -95,10 +95,7 @@
   (file-exists-p (expand-file-name ".zeus.sock" root)))
 
 (defun rake--bundler-p (root)
-  (cond
-   ((file-exists-p (expand-file-name "Gemfile" root)) t)
-   ((equal (expand-file-name root) "/") nil)
-   (t (rake--bundler-p (concat (file-name-as-directory root) "..")))))
+  (locate-dominating-file root "Gemfile"))
 
 (defun rake--vertical-ido-on-p ()
   (and

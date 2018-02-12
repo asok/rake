9c204334b03b4e899fadae6e59c20cf105404128
Adam Sokolnicki <adam.sokolnicki@gmail.com>
2018-02-12 11:08:03 +0100

    Bumb version number

--- rake.el
+++ rake.el
@@ -4,7 +4,7 @@
 
 ;; Author:            Adam Sokolnicki <adam.sokolnicki@gmail.com>
 ;; URL:               https://github.com/asok/rake.el
-;; Version:           0.3.3
+;; Version:           0.4.2
 ;; Keywords:          rake, ruby
 ;; Package-Requires:  ((f "0.13.0") (dash "1.5.0") (cl-lib "0.5"))
 
